f2b4374023b9547afdb547828cd3c3ce52d27c4b
MM-TechLab <142767812+MalcolmGit2023@users.noreply.github.com>
2025-12-19 12:31:57 -0500

    Update EA_MAU_Deferrals.sh

--- mau-update-suite/jamf/EA_MAU_Deferrals.sh
+++ mau-update-suite/jamf/EA_MAU_Deferrals.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-STATE_FILE="/Library/Application Support/BHG/MAU/state.txt"
+STATE_FILE="/Library/Application Support/MAU/state.txt"
 COUNT=$(awk -F= '/^deferrals=/{print $2}' "$STATE_FILE" 2>/dev/null)
 [[ -z "$COUNT" ]] && COUNT=0
 echo "<result>${COUNT}</result>"
