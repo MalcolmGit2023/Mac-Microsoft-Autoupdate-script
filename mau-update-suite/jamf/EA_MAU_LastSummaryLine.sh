d5e36e76b30e26abdd10a5adcb1d2841bf3c3fbe
MM-TechLab <142767812+MalcolmGit2023@users.noreply.github.com>
2025-12-19 12:32:10 -0500

    Update EA_MAU_LastSummaryLine.sh

--- mau-update-suite/jamf/EA_MAU_LastSummaryLine.sh
+++ mau-update-suite/jamf/EA_MAU_LastSummaryLine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILE="/Library/Logs/BHG/mau_update_summary.txt"
+FILE="/Library/Logs/mau_update_summary.txt"
 [[ -f "$FILE" ]] || { echo "<result>none</result>"; exit 0; }
 LAST=$(tail -n 1 "$FILE")
 echo "<result>${LAST}</result>"
